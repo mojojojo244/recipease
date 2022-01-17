96d0de6c373ea62f65b1aafe17914f6e3d678f4b
mojojojo244 <jordan.crutcher@gmail.com>
2022-01-17 15:38:45 -0600

    user model created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,22 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
   res.render('signup', { title: 'Sign Up' });
 };
 module.exports.login_get = (req, res) => {
   res.render('login', { title: 'Login' });
 };
-module.exports.signup_post = (req, res) => {
-  const { email, password } = req.body;
-  res.send('new signup');
+module.exports.signup_post = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.create({ username, password });
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send('error, user not created');
+  }
 };
-module.exports.login_post = (req, res) => {
-  const { email, password } = req.body;
+module.exports.login_post = async (req, res) => {
+  const { username, password } = req.body;
   res.send('user login');
 };
