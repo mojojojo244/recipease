bf6b0c3bfa01b4e961cd60e134aa1698d200477b
mojojojo244 <jordan.crutcher@gmail.com>
2022-01-17 13:28:58 -0600

    basic server implemented

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const dbURI =
+  'mongodb+srv://jojo:erterbernds@recipease.mna68.mongodb.net/recipease?retryWrites=true&w=majority';
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
